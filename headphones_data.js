10ef1437cd7f54b97f0626406a8ac2c2097e4659
Mayank Varshney <tikaramvarshney566@gmail.com>
2022-09-04 22:49:42 +0530

    Sample UI created

---
+++ headphones_data.js
@@ -0,0 +1,163 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": " ",
+    "description": " ",
+    "brand": " ",
+    "image": "./image/1.jpg",
+    "head": " ",
+    "battery": " ",
+
+
+}, {
+
+    "id": 1,
+    "title": "boAt Rockerz 510 Super Extra Bass Bluetooth Headset  (Raging Red, On the Ear)",
+    "price": "1099 ",
+    "description": " If you are looking for an efficient wireless headset, . Moreover, you can enjoy up to 10 hours of playback time with a fully charged battery. Furthermore,.",
+    "brand": "Boat ",
+    "image": "./image/1.jpg",
+    "head": "Wireless",
+    "battery": "No ",
+}, {
+
+    "id": 2,
+    "title": "realme Techlife Buds T100 ",
+    "price": " 1499",
+    "description": " Wireless range: 10 m Battery life: 28 hrs | Charging time: 1.30 hrs 10mm Dynamic Bass Driver for Real HD Sound & AI ENC for Calls Up to 28hrs Total Playback with Fast charging - 10min charge for up to 120min playbac Mode with 88ms Super Low Latency & Google Fast Pair Instant Connection",
+    "brand": " realme",
+    "image": "./image/2.jpg",
+    "head": "Wireless ",
+    "battery": "yes ",
+}, {
+
+    "id": 3,
+    "title": "realme Buds Air 3 Neo ",
+    "price": " 1799",
+    "description": "Enjoy flawless sound clarity and groove into your favourite song with the realme Air 3 Neo Bluetooth Headset that drives you towards excellence. ",
+    "brand": " realme",
+    "image": "./image/3.jpg",
+    "head": "Wireless ",
+    "battery": "yes",
+}, {
+
+    "id": 4,
+    "title": " boAt Rockerz 650 ",
+    "price": " 1799",
+    "description": " best productBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss. ",
+    "brand": "boat ",
+    "image": "./image/4.jpg",
+    "head": "Wireless ",
+    "battery": " yes",
+}, {
+
+    "id": 5,
+    "title": " boAt RockerBoult Audio Bassbuds X1 Wired Headsetz 650 ",
+    "price": " 329",
+    "description": " be Enjoy listening to your favourite tracks with this Boult Audio X1 Wired Headset with Mic. Featuring ergonomically designed AL-grade drivers, one-button remote microphone and Kevlar cables, this headset is built to last and ensures an immersive aural experience.st productBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. ",
+    "brand": "boat ",
+    "image": "./image/5.jpg",
+    "head": "Wired ",
+    "battery": " No",
+}, {
+
+    "id": 6,
+    "title": " boAt RockboAt Bassheads 103 Wired Headset  (Mint Blue, In the Ear) erz 650 ",
+    "price": " 499",
+    "description": " best product Access musical Nirvana and get hooked to the bass heavy sound of the Bassheads 103. A carefully crafted design with casual elegance in abundance, choose your color that sets your vibe and begin the journey to the shores of Nirvana. Connect to all types of devices with 3.5mm jack and enjoy a secure fit comfortable listening experience with the perfect length tangle free cable. Its Super Extra Bass on hand all the time with the boAt Bassheads 103, the earphones to your Nirvana. Equipped with an in-line mic and voice assistant feature, access easy entry to any/all calls and queries and pause/play music at your convenience. Its time to GET HOOKED to the sound, look and features of the boAt Bassheads 103ctBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss.",
+    "brand": "boat ",
+    "image": "./image/6.jpg",
+    "head": "Wired ",
+    "battery": "No ",
+}, {
+
+    "id": 7,
+    "title": " JBL C150SI with One Button Universal Remote Wired Headset  (Black, In the Ear) Rockerz 650 ",
+    "price": " 699",
+    "description": " best prYou can enjoy the sound of deepened notes with stellar bass response and manage your calls conveniently with the JBL C150SI wired headset. It features a one-button universal remote that helps you operate this headset with ease. Moreover, it is equipped with a noise-isolation microphone that enables the recipient of your call to hear your voice clearly without any disturbance caused by the noise in the background.oductBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss. ",
+    "brand": "JBL",
+    "image": "./image/7.jpg",
+    "head": "Wired ",
+    "battery": "No ",
+}, {
+
+    "id": 8,
+    "title": " OnePlus Bullets Wireless Z2 with Fast Charge, ",
+    "price": " 1999",
+    "description": " Bullets Wireless Z2 Magico Black Headphone Type In the Ear1 Neckband, Type C Cable, Additional Pair of Eartips, User Guide, Warranty CardConnectivity Bluetooth Headphone DesignBehind the NeckoductBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss. Its 40mm dynamic drivers help supply superior musical experience to the user with immersive sound. With our ASAP fast charge technology, you can get the headphones fully charged in just 60 minutes. It also offers EQ modes as well as Smart Twist feature for a superior playback experience",
+    "brand": "OnePLus ",
+    "image": "./image/8.jpg",
+    "head": "Wireless ",
+    "battery": "Yes ",
+}, {
+
+    "id": 9,
+    "title": "Earboss M10 Earbuds/ipod/buds 5.1 Earbuds with 280H Playtime ",
+    "price": " 899",
+    "description": " best Highnotes UPGRADED VERSION M10 TWS Wireless Headphones with Cancellation and Extra Bass Fit-in Ear Earphones are the current generation earphones with 9D Hifi Sound Quality and a 2000 mAh Battery and they also can be used as POWER BANK to charge up your Smartphones. The True wireless headphones Bluetooth V5.1 earphones headphones  Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss. Its 40mm dynamic drivers help supply superior musical experience to the user with immersive sound. With our ASAP fast charge technology.",
+    "brand": "Earboss ",
+    "image": "./image/9.jpg",
+    "head": "Wireless ",
+    "battery": "Yes",
+}, {
+
+    "id": 10,
+    "title": " boAt RboAt Rockerz 450 Proockerz 650 ",
+    "price": " 1999",
+    "description": " boAt Rockerz 450 Pro is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Apart from the wireless connectivity, one can utilize the AUX cable as well for a wired playback experience. productBoAt Rockerz 650 is a power-packed on-ear wireless headphone that has been ergonomically designed to meet the needs of music lovers. The headphones come equipped with Bluetooth V5.0 for instant wireless connectivity. Its super powerful 1000mAh battery provides a humongous playtime of up to 60 hours for an extended audio bliss. Its 40mm dynamic drivers help supply superior musical experience to the user with immersive sound. With our ASAP fast charge technology, you can get the headphones fully charged in just 60 minutes. ",
+    "brand": "boat ",
+    "image": "./image/10.jpg",
+    "head": "Wireless ",
+    "battery": "Yes",
+}, {
+
+    "id": 11,
+    "title": "AIRSOUND X300 WIRELESS BLUETOOTH IN-EAR HEADPHONES ",
+    "price": " 839",
+    "description": " Revolutionary auto-pause when the magnetic earbuds are connected and auto-play when pulled apart AirSound x300 compatible with Bluetooth enabled device/smartphone including iPhone, android, windows, mac and more",
+    "brand": "AirSound ",
+    "image": "./image/11.jpg",
+    "head": "Wired ",
+    "battery": "Yes",
+}, {
+
+    "id": 12,
+    "title": "Earboss M10 EaANT ESPORTS H1000 PRO WIRED OVER EAR HEADPHONES WITH MIC (BLACK)rbuds/ipod/buds 5.1 Earbuds with 280H Playtime ",
+    "price": " 899",
+    "description": " Model H1000 BlackModel Name	H1000Product Dimensions	.8 x 9.8 x 20.8 cm; 550 GramsItem model number H1000 BlackHardware Platform Gaming Console, PC, Smartphone.",
+    "brand": "Ant Esports ",
+    "image": "./image/12.jpg",
+    "head": "Wireless ",
+    "battery": "Yes",
+}, {
+
+    "id": 13,
+    "title": "Earboss M10 Earbuds/ipod/buds 5.1 Earbuds with 280H ASUS ROG CETRA IN-EAR GAMING HEADPHONES WITH ACTIVE NOISE CANCELLATION ",
+    "price": " 4999",
+    "description": "Cetra are USB-C gaming earbuds that support multiple platforms, including mobile phones, PC, Mac and Nintendo Switch. Featuring Active Noise Cancellation (ANC) technology and the innovative Ambient mode, ROG Cetra lets you fully immerse yourself in your games, music and other ",
+    "brand": "ASUS ",
+    "image": "./image/13.jpg",
+    "head": "Wired ",
+    "battery": "NO",
+}, {
+
+    "id": "14",
+    "title": "Earboss BOAT ROCKERZ 370 BLUETOOTH WIRELESS ON EAR HEADPHONES M10 Earbuds/ipod/buds 5.1 Earbuds with 280H Playtime ",
+    "price": " 749",
+    "description": " Battery: Rockerz 370 offers a playback time of up to 12 hours. Playback Time : 12 hours, Charging Time : 2-3 hours, Standby Time : 180 hoursBluetooth: It has Bluetooth v5.0 with a range of 10m and is compatible with Android & iOS",
+    "brand": "Boat ",
+    "image": "./image/14.jpg",
+    "head": "Wireless ",
+    "battery": "Yes",
+}, {
+
+    "id": "15",
+    "title": "BOSE NOISE CANCELLING WIRELESS BLUETOOTH HEADPHONES BLACK",
+    "price": "26999",
+    "description": "Keep your phone in your pocket and your head up to the world with easy access to voice assistants for music, navigation, weather and more ",
+    "brand": "Bose",
+    "image": "./image/15.jpg",
+    "head": "Wireless ",
+    "battery": "Yes",
+},
+];
